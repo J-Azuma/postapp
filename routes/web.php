e191837746091d8966e86efa2bd7049840900b64
J-azuma <j-azuma@LAPTOP-MTNJ7R56.localdomain>
2020-07-01 07:54:47 +0900

    ルーティングを追加。投稿一覧画面はホーム画面を兼任

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PostController@index')->name('home');
+Route::get('/posts/index','PostController@index')->name('posts.index');
+
