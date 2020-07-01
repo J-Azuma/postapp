6e5ede5df6d193aaf4789a304612129d5cd67074
J-azuma <j-azuma@LAPTOP-MTNJ7R56.localdomain>
2020-07-01 13:17:20 +0900

    投稿作成用のルーティング

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'PostController@index')->name('home');
 Route::get('/posts/index','PostController@index')->name('posts.index');
+Route::get('/posts/create', 'PostController@showCreateForm')->name('posts.create');
+Route::post('/posts/create', 'PostController@create');
 
