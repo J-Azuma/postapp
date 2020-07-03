d6dc030a58963394128a75dc2e2b68a26f61485e
J-azuma <j-azuma@LAPTOP-MTNJ7R56.localdomain>
2020-07-03 13:07:15 +0900

    ポスト作成用ページを作成

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  <h3>Add Post</h3>
+  <form action="#" method="post">
+    <label for="post-title">title</label> <br>
+    <input type="text" id="post-title" name="title"> <br>
+    <label for="post-content"></label> <br>
+    <textarea name="content" id="post-content" cols="20" rows="10"></textarea> <br>
+    <button>submit</button>
+  </form>
+</body>
+</html>
