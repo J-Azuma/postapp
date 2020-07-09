6ed00e597511eac3877a7fe05626bb5fc74349d4
J-azuma <j-azuma@LAPTOP-MTNJ7R56.localdomain>
2020-07-10 08:23:14 +0900

    postsテーブルとusersテーブルの関連付けを実装

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -13,10 +13,12 @@ class PostsTableSeeder extends Seeder
      */
     public function run()
     {
+      $user = DB::table('users')->first();
         foreach (range(1,20) as $num) {
           DB::table('posts')->insert([
             'title' => "title {$num}",
             'content' => "dummy: {$num} \n test test test test test test unko nyaan",
+            'user_id' => $user->id,
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
           ]);
