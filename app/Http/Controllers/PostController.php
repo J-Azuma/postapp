3ba93a7435b9368b23f2c677c76eb090cc5f78d3
J-azuma <j-azuma@LAPTOP-MTNJ7R56.localdomain>
2020-07-11 21:36:49 +0900

    投稿作成ページを削除したため、このページに遷移する機能は不要になった

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -22,15 +22,6 @@ class PostController extends Controller
     ]);
   }
 
-  /**
-   * post作成用ページを表示.
-   *
-   * @return 投稿作成用フォーム
-   */
-  public function showCreateForm()
-  {
-    return view('posts.create');
-  }
 
   /**
    * 投稿作成を行う.
