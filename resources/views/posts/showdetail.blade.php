905cf5630affbbcbe96085f158dc5ef0626f8ff9
J-azuma <j-azuma@LAPTOP-MTNJ7R56.localdomain>
2020-07-11 21:23:52 +0900

    自分のポストにはコメントを付けられない仕様に合わせて、ログインユーザーのidとポストのuser_idが異なる場合にのみ、コメント作成フォームを表示する

--- resources/views/posts/showdetail.blade.php
+++ resources/views/posts/showdetail.blade.php
@@ -9,6 +9,8 @@
 <body>
   <p>タイトル</p>
   <span>{{$post->title}}</span> <br>
+  <p>投稿者</p>
+  <span>{{App\User::find($post->user_id)->name}}</span> <br>
   <p>作成日</p>
   <span>{{$post->created_at->format('yy/m/d')}}<span>
   <p>本文</p>
@@ -24,12 +26,14 @@
     <hr>
   @endforeach
 
+  @if (Auth::check() && $post->user_id != Auth::user()->id)
   コメントを送信する <br>
   <form action="{{route('comments.create', ['post' => $post])}}" method="post">
    @csrf
    <label for="comment">本文</label> <br>
    <textarea name="content" id="comment" cols="20" rows="10">{{old('content')}}</textarea> <br>
    <button>コメントを送信</button>
+  @endif
   </form>
 </body>
 </html>
