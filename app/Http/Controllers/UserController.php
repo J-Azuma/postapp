4dd79167aceb91333527a6db57e81811b72ea258
J-azuma <j-azuma@LAPTOP-MTNJ7R56.localdomain>
2020-07-12 11:10:54 +0900

    ユーザー詳細画面表示用のコントローラメソッドを追加

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\User;
+
+class UserController extends Controller
+{
+
+  /**
+   * ユーザー詳細画面へのルーティング
+   *
+   * @param User $user
+   * @return void
+   */
+  public function showDetail(User $user)
+  {
+    return view('users.showDetail', [
+      'user' => $user,
+    ]);
+  }
+}
