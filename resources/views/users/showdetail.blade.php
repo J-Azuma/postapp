a7b16775d7452589df30c0b2f4bed6227af15612
J-azuma <j-azuma@LAPTOP-MTNJ7R56.localdomain>
2020-07-12 11:41:04 +0900

    自分のユーザー情報表示画面から、ユーザー情報編集画面に遷移できるように改修

--- resources/views/users/showdetail.blade.php
+++ resources/views/users/showdetail.blade.php
@@ -9,6 +9,10 @@
 <body>
   name: {{$user->name}} <br>
   register date :{{$user->created_at->format('yy/m/d')}} <br>
-  profile : {{$user->profile}}
+  profile : {{$user->profile}} <br>
+
+  @if (Auth::check() && Auth::user()->id == $user->id)
+  <a href="{{route('users.showeditform', ['user' => $user])}}">登録情報を編集する</a>
+  @endif
 </body>
 </html>
